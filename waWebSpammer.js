bd81b531aadbc771980d72fa01796536907c4fcf
Ueda-Ichitaka <Ueda-Ichitaka@users.noreply.github.com>
2016-07-11 20:09:19 +0200

    Random Time and Emote Update
    Simple WhatsApp Web spam script. Sends random Emoji with given text from line 18 in random time delays.

--- waWebSpammer.js
+++ waWebSpammer.js
@@ -1,7 +1,8 @@
-var msgcount = 25;
-var delay = 600;
-var emojiTab = 7;	/* There is no 0, from 1 to 6. */
-var emojiNumber = 23  ;	/* No 0 either. */
+var msgcount = 10;
+var delay = Math.floor((Math.random() * 500) + 200);
+var emojiTab = Math.floor((Math.random() * 9) + 1);	/* There is no 0, from 1 to 6. */
+var emojiNumber = Math.floor((Math.random() * 25) + 1)  ;	/* No 0 either. */
+var spamText = "Spam!";
 /* The actual code */
 function getElementByXpath(path) { return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue; }
 var winnniey = 0;
@@ -14,7 +15,7 @@ function sendMessage() {
 	tab.click();
    	setTimeout(function () {
    		var emoji = getElementByXpath('//*[@id="main"]/footer/span/div/span/div/div/span[' + emojiNumber + ']');
-	  	document.getElementsByClassName("input")[1].innerHTML="Würflein anrufen!";
+	  	document.getElementsByClassName("input")[1].innerHTML="Spam!";
 		emoji.click();
 	  	setTimeout(function () {
 		  	var sendButton = getElementByXpath('//*[@id="main"]/footer/div/button[2]');
@@ -22,7 +23,11 @@ function sendMessage() {
 			setTimeout(function () {
 			    winnniey++;        
 			    if (winnniey < msgcount) {
-			    	sendMessage();   
+					delay = Math.floor((Math.random() * 500) + 500);
+					emojiTab = Math.floor((Math.random() * 9) + 1);	/* There is no 0, from 1 to 6. */
+					emojiNumber = Math.floor((Math.random() * 25) + 1)  ;	/* No 0 either. */
+			    	console.log(delay);
+					sendMessage();   
 			    }
 		    }, delay);
 		}, delay);
